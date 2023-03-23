909aedbbcaaf3371f06c1f491fe516e1ddf6dda2
winxene <winxeneric@gmail.com>
2023-03-23 22:24:32 +0700

    feat: added telescope and vscode neovim support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,3 +4,18 @@ require("ikura.core.keymaps")
 require("ikura.core.colorscheme")
 require("ikura.plugins.comment")
 require("ikura.plugins.nvim-tree")
+require("ikura.plugins.lualine")
+require("ikura.plugins.telescope")
+
+if vim.g.vscode then
+    -- VSCode extension
+    require("ikura.plugins-setup")
+    require("ikura.core.options")
+    require("ikura.core.keymaps")
+    require("ikura.core.colorscheme")
+    require("ikura.plugins.comment")
+    require("ikura.plugins.nvim-tree")
+    require("ikura.plugins.lualine")
+else
+    -- ordinary Neovim
+end
