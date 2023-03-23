965055c6cc82e79f5944a32231c0aeec86b0d59a
winxene <winxeneric@gmail.com>
2023-03-23 19:30:24 +0700

    uploaded nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,5 @@
+require("ikura.plugins-setup")
+require("ikura.core.options")
+require("ikura.core.keymaps")
+require("ikura.core.colorscheme")
+
---
+++ .config/nvim/lua/ikura/core/keymaps.lua
@@ -0,0 +1,18 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+-- general keymaps
+-- will add more soon
+
+--[[
+
+the pattern:
+
+keymap.set("n", "shortcuts you want to create", "the running command")
+
+- <CR> == return button/ enter button
+- <C-something> == control + something
+
+]]--
+
