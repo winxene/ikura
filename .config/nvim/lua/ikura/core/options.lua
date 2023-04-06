863827570f129ce42239cbce6254b58955bb24f6
winxene <winxeneric@gmail.com>
2023-04-06 20:49:07 +0700

    chore: changing settings for shell, line numbers, and tabs and indentation

--- .config/nvim/lua/ikura/core/options.lua
+++ .config/nvim/lua/ikura/core/options.lua
@@ -8,19 +8,38 @@ opt.number = true
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
+opt.smarttab = true
 opt.autoindent = true
+opt.smartindent = true
+
+opt.cmdheight = 1
+opt.title = true
+
+-- shell
+opt.shell = "/bin/zsh"
 
 -- line wrapping
 opt.wrap = false
+opt.backspace = "indent,eol,start"
 
 -- search settings
 opt.ignorecase = true
 opt.smartcase = true
+opt.path:append { "**" }
+opt.wildignore:append(
+	"*/tmp/*,*.so,*.swp,*.zip",
+	"*/node_modules/*",
+	"*/.git/*",
+	"*/.cache/*",
+	"*/.vscode/*",
+	"*/.idea/*",
+	"*/"
+)
 
 -- cursor line
 opt.cursorline = true
 
--- appearance 
+-- appearance
 opt.termguicolors = true
 opt.background = "dark"
 opt.signcolumn = "yes"
