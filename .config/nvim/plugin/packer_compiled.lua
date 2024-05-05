c10b0c72ad31fc7cb4d5292fff07cc30a71ffe65
winxene <winxeneric@gmail.com>
2024-05-06 00:17:44 +0700

    feat: added presence

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -231,6 +231,11 @@ _G.packer_plugins = {
     path = "/Users/ikura/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  ["presence.nvim"] = {
+    loaded = true,
+    path = "/Users/ikura/.local/share/nvim/site/pack/packer/start/presence.nvim",
+    url = "https://github.com/andweeb/presence.nvim"
+  },
   ["telescope-fzf-native.nvim"] = {
     loaded = true,
     path = "/Users/ikura/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
