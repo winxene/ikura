c659a1bf164d57ca3d5583637d55b04026c0c623
winxene <winxeneric@gmail.com>
2024-12-09 12:43:13 +0700

    feat: added arduino and kotlin to treesitter

--- .config/nvim/lua/ikura/plugins/presence.lua
+++ .config/nvim/lua/ikura/plugins/presence.lua
@@ -12,8 +12,8 @@ presence.setup({
 		"/tmp/",
 		"/private/tmp/",
 		"/var/folders/",
-		"/Users/ikura/Development/",
-		"/Users/ikura/",
+		-- "/Users/ikura/Development/",
+		-- "/Users/ikura/",
 	}, -- A list of strings that will disable Presence if the current file path matches
 	-- client_id = "793271441293967371",
 	buttons = false,
@@ -30,10 +30,10 @@ presence.setup({
 		local filetype = vim.bo.filetype:gsub("^%l", string.upper)
 		return string.format("Editing %s", filetype)
 	end,
-	file_explorer_text = "Browsing %s", -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+	file_explorer_text = "Browsing work stuffs", -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
 	git_commit_text = "Committing changes", -- Format string rendered when committing changes in git (either string or function(filename: string): string)
 	plugin_manager_text = "Managing plugins", -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
-	reading_text = "Reading %s", -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
-	workspace_text = "Working on %s", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+	reading_text = "", -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+	workspace_text = "", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
 	line_number_text = "Line %s out of %s",
 })
