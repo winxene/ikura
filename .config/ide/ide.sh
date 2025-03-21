f94cc17e289d8f4361b36ee547c46a1983a19bc2
winxene <winxeneric@gmail.com>
2025-03-21 11:36:35 +0700

    feat: added the alternative layout

--- .config/ide/ide.sh
+++ .config/ide/ide.sh
@@ -8,19 +8,36 @@ if [ "$1" == "exit" ]; then
   exit 0
 fi
 
+if [ "$1" == "--alt" ]; then
+  if tmux has-session -t "$session_name" 2>/dev/null; then
+    tmux attach-session -t "$session_name"
+    exit 0
+  fi
+
+  tmux new-session -d -s "$session_name"
+  tmux select-pane -t 0
+  tmux split-window -h -p 30
+  tmux split-window -v -p 60
+  tmux select-pane -t 0
+  tmux send-keys 'nvim' C-m
+  tmux attach-session -t "$session_name"
+  exit 0
+fi
+
 if tmux has-session -t "$session_name" 2>/dev/null; then
   tmux attach-session -t "$session_name"
   exit 0
 fi
 
 tmux new-session -d -s "$session_name"
+tmux split-window -v -p 15
 
-tmux select-pane -t 0
-tmux split-window -h -p 30
+tmux select-pane -t 1
 
-tmux split-window -v -p 60
+tmux split-window -h -p 66
+tmux select-pane -t 2
+tmux split-window -h -p 50
 
 tmux select-pane -t 0
 tmux send-keys 'nvim' C-m
-
 tmux attach-session -t "$session_name"
