3fed775f987be7d7516a0f7787de417ecf9da238
winxene <winxeneric@gmail.com>
2025-05-14 15:53:16 +0700

    fix: fixed the issue by replacing the dependencies to none-ls

--- .config/nvim/lua/ikura/plugins/lsp/null-ls.lua
+++ .config/nvim/lua/ikura/plugins/lsp/null-ls.lua
@@ -30,12 +30,7 @@ null_ls.setup({
 		formatting.rustfmt,
 
 		formatting.stylua, -- lua formatter
-		diagnostics.eslint_d.with({ -- js/ts linter
-			-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
-			condition = function(utils)
-				return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
-			end,
-		}),
+    require("none-ls.diagnostics.eslint_d"),        
 	},
 	-- configure format on save
 	on_attach = function(current_client, bufnr)
