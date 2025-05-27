f2f25ea610325989fdb187b0d8d3b041de0a2d20
winxene <winxeneric@gmail.com>
2025-05-27 15:20:48 +0700

    feat: added some customisations for lualine

--- .config/nvim/lua/ikura/plugins/lualine.lua
+++ .config/nvim/lua/ikura/plugins/lualine.lua
@@ -6,9 +6,19 @@ end
 
 local lualine_monokai_pro = require("monokai-pro")
 
--- configure lualine with modified theme
-lualine.setup({
+-- set up lualine
+require("lualine").setup({
   options = {
     theme = 'monokai-pro',
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch" },
+    lualine_c = {
+      { "filename", path = 1 },
+    },
+    lualine_x = { "encoding", "fileformat", "filetype" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
   },
 })
